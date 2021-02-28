bf6ce70f3f7336c5685ab0e642fae247a1641ef4
Zwusel <schlonz2k@abwesend.de>
2021-02-28 16:02:17 +0100

    Add style_compatible directory in ressources and move rp_tilesets to that location;

---
+++ ressources/style_compatible/rp_tilesets/tsx/rp_exterior_wall_floor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="rp_exterior_wall_floor" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../rp_exterior_wall_floor.png" width="512" height="512"/>
+</tileset>
