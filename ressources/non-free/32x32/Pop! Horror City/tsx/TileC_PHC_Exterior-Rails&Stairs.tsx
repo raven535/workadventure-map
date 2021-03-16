50bfbe3fff4a506ec11ea3865ab9af8da2339880
Zwusel <schlonz2k@abwesend.de>
2021-03-16 20:39:26 +0100

    Mainly removed non-free tilesets and reorganized style-compatible;
    rp_tilesets_alpha: fast approach...using only ready to go tilesets
    rp_tilesets_beta: use custom tilesets to optimize the maps

--- ressources/non-free/32x32/Pop! Horror City/tsx/TileC_PHC_Exterior-Rails&Stairs.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="TileC_PHC_Exterior-Rails&amp;Stairs.tsx" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="../TileC_PHC_Exterior-Rails&amp;Stairs.png" width="512" height="512"/>
-</tileset>
